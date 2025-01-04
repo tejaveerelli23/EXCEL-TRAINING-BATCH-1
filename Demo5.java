ba4b8306668579f48252233655b07143a390ac54
tejaveerelli23 <verellitejateja@gmail.com>
2025-01-04 11:26:23 +0530

    Add files via upload

---
+++ Demo5.java
@@ -0,0 +1,9 @@
+public class Demo5 {
+    public static void main(String[] args) {
+        // Using a for loop to print numbers from 1 to 100
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
