16a9da4e8a418f159ddb19d569755fbed91f4a37
tejaveerelli23 <verellitejateja@gmail.com>
2025-01-06 12:27:14 +0530

    Add files via upload

---
+++ Demo6.java
@@ -0,0 +1,13 @@
+public class Demo6 {
+    public static void main(String[] args){
+        //verify user credentials username = user001 pwd = user_001
+        String username = "user001";
+        String password = "user_001";
+
+        while((username == "user001") && (password == "user_001")) {
+            System.out.println("hello user happy new year");
+            break;
+        }
+    }
+}
+
